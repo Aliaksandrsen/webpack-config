39295188b6a5975f5bbef417ddc7a5ab16d912e7
Aliaksandrsen <puskin1914@gmail.com>
2020-04-05 11:17:40 +0300

    feat: webpack config for different modes

--- webpack.config.js
+++ webpack.config.js
@@ -1,72 +1,108 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  mode: "development",
 
+module.exports = (env = {}) => {
+  const { mode = 'development' } = env; //по умолчанию чтобы равно development
 
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-      },
-      // images
-      {
-        test: /\.(png|jpg|jpeg|ico)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              outputPath: 'images',
-              name: '[name]-[sha1:hash:7].[ext]'
-            }
-          }
-        ]
-      },
-      // fonts
-      {
-        test: /\.(ttf|otf|eot|woff|woff2)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              outputPath: 'fonts',
-              name: '[name].[ext]'
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(css)/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.(s[ca]ss)/,
-        use: [
-          MiniCssExtractPlugin.loader,// добавление css на страницу
-          'css-loader',
-          'sass-loader', // лоадкры работают с конца
-        ]
-      },
+  const isProd = env.mode === 'production';
+  const isDev = env.mode === 'development';
+
+  const getStyleLoaders = () => {
+    return [
+      isProd ? MiniCssExtractPlugin.loader : 'style-loader',
+      'css-loader'
     ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'New title',
-      buildtime: new Date().toString(),
-      template: 'public/index.html'
-    }),
-    new MiniCssExtractPlugin({ 
-      filename: 'main-[hash:8].css'
-    })
-  ],
-  devServer: {
-    open: true,
-    
-  },
+  };
+
+  const getPlugins = () => {
+    const plugins = [
+      new HtmlWebpackPlugin({
+        title: 'New title',
+        buildtime: new Date().toString(),
+        template: 'public/index.html'
+      })
+    ];
+
+    if (isProd) {
+      plugins.push(
+        new MiniCssExtractPlugin({
+          filename: 'main-[hash:8].css'
+        })
+      )
+    }
+
+    return plugins;
+  };
+
+  return {
+    mode: isProd ? 'production' : isDev && 'development',
+
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader'
+        },
+        // images
+        {
+          test: /\.(png|jpg|jpeg|ico)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                outputPath: 'images',
+                name: '[name]-[sha1:hash:7].[ext]'
+              }
+            }
+          ]
+        },
+        // fonts
+        {
+          test: /\.(ttf|otf|eot|woff|woff2)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                outputPath: 'fonts',
+                name: '[name].[ext]'
+              }
+            }
+          ]
+        },
+        {
+          test: /\.(css)/,
+          use: getStyleLoaders(), // выбор css loader взавис от mode
+        },
+        {
+          test: /\.(s[ca]ss)/,
+          //? ============================ убираем внизу ф-я
+          // use: [
+          //   MiniCssExtractPlugin.loader,// добавление css на страницу
+          //   'css-loader',
+          //   'sass-loader', // лоадкры работают с конца
+          // ],
+          //? ============================
+          use: [...getStyleLoaders(), 'sass-loader']
+        },
+      ]
+    },
+    //? ============================ убираем внизу ф-я
+    // plugins: [
+    //   new HtmlWebpackPlugin({
+    //     title: 'New title',
+    //     buildtime: new Date().toString(),
+    //     template: 'public/index.html'
+    //   }),
+    //   new MiniCssExtractPlugin({
+    //     filename: 'main-[hash:8].css'
+    //   })
+    // ],
+    //? ============================
+    plugins: getPlugins(),
+    devServer: {
+      open: true,
+    },
+  }
 };
